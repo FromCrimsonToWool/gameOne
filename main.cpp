8437253a18281ff441d04a98ff0b944ae01a02d7
Casey Bird <FromCrimsonToWool+Git@Gmail.com>
2015-09-14 14:02:51 -0400

    Drawing GIF on screen

--- main.cpp
+++ main.cpp
@@ -1,14 +1,120 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
 
-/*
- * Lesson 0: Test to make sure SDL is setup properly
- */
+//Screen dimension constants
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//Loads individual image
+SDL_Surface* loadSurface( std::string path );
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+	
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//Current displayed PNG image
+SDL_Surface* gCharacterSprite = NULL;
+
+bool init(){
+    bool success = true;
+
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    } else {
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL ){
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        } else {
+            int imgFlags = IMG_INIT_PNG;
+            if( !( IMG_Init( imgFlags ) & imgFlags )){
+                printf( "SDL_image  could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                success = false;
+            } else {
+                gScreenSurface = SDL_GetWindowSurface( gWindow );
+            }
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia(){
+
+    bool success = true;
+    gCharacterSprite = loadSurface( "res/cotwsprites02.gif" );
+
+    if( gCharacterSprite == NULL ){
+        printf( "Failed to load character sprite image.\n" );
+        success = false;
+    }
+
+    return success;
+}
+
+SDL_Surface* loadSurface( std::string path ){
+   SDL_Surface* optimizedSurface = NULL;
+
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL ){
+        printf( "Unable to load image %s!  SDL_image Error:  %s\n", path.c_str(), IMG_GetError() );
+    } else {
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, NULL );
+        if( optimizedSurface == NULL ){
+            printf( "Unable to optimize image %s!  SDL_Error:  %s\n", path.c_str(), SDL_GetError() );
+        }
+
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return optimizedSurface;
+}
+
+void close(){
+    SDL_FreeSurface( gCharacterSprite );
+    gCharacterSprite = NULL;
+
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    IMG_Quit();
+    SDL_Quit();
+}
+
 int main(int, char**){
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-	SDL_Quit();
-	return 0;
+    if( !init() ){
+        printf( "Failed to initialize!\n" );
+    } else {
+        if( !loadMedia() ){
+            printf( "Failed to load media!\n" );
+        } else {
+            bool quit = false;
+            SDL_Event e;
+
+            while(!quit){
+               while( SDL_PollEvent( &e ) != 0 ){
+                  if( e.type == SDL_QUIT ){ quit = true; }
+               }
+
+                SDL_BlitSurface( gCharacterSprite, NULL, gScreenSurface, NULL );
+                SDL_UpdateWindowSurface( gWindow );
+            }
+        }
+    }
+
+    close();
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CXX = clang++
-SDL = -framework SDL2
+SDL = -framework SDL2 -framework SDL2_image
 # If your compiler is a bit older you may need to change -std=c++11 to -std=c++0x
 CXXFLAGS = -F/Library/Frameworks -Wall -c -std=c++11
 LDFLAGS = -F/Library/Frameworks $(SDL)
@@ -8,10 +8,10 @@ EXE = Castle_of_the_Winds-_Universal
 all: $(EXE)
 
 $(EXE): main.o
-	$(CXX) $(LDFLAGS) $< -o bin/$@
+	$(CXX) $(LDFLAGS) bin/$< -o bin/$@
 
 main.o: main.cpp
 	$(CXX) $(CXXFLAGS) $< -o bin/$@
 
 clean:
-	rm *.o && rm $(EXE)
+	rm bin/*.o && rm bin/$(EXE)
