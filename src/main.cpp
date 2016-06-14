9f6bd5f9f2014069f040601751ef25f0d3eaf905
Casey B <FromCrimsonToWool@GMail.com>
2016-06-14 08:45:33 -0400

    Modified SDL header

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
+#include <SDL2/SDL_image.h>
 #include <src/res_path.h>
 
 //Screen dimension constants
